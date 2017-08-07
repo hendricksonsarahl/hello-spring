db5a33a0688a67899c4777d0e847011e7d584a97
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-08-07 16:05:36 -0700

    Updated HelloController file, unignore src file

--- src/main/java/com/example/hellospring/controllers/HelloController.java
+++ src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hellospring.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +11,10 @@ public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+
+        String username = request.getParameter("name")
+        return "Hello" + username;
     }
 
     @RequestMapping(value = "goodbye")
