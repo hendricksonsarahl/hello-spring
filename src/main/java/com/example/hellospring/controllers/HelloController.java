b3763c9d943277491e0f65708c733be4699b6044
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-08-08 10:54:42 -0700

    Class 3.3 studio progress so far

--- src/main/java/com/example/hellospring/controllers/HelloController.java
+++ src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -8,8 +8,61 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+/* Move createMessage to it's own class file OUTSIDE of the controllers directory
+* */
+
 @Controller
 public class HelloController {
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm(){
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "</form>" +"<select name='greeting'>" +
+                "<option value='English'>English</option>" +
+                "<option value='Spanish'>Spanish</option>" +
+                "<option value='French'>French</option>" +
+                "<option value='German'>German</option>" +
+                "<option value='Klingon'>Klingon</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+        return html;
+    }
+
+        public static String createMessage(String lang) {
+
+            String greeting = "";
+            if(lang.equals("English")){
+                greeting = "Hello ";
+            } else if (lang.equals("Spanish")){
+                greeting = "Hola ";
+            } else if (lang.equals("French")){
+                greeting = "Bonjoir ";
+            } else if (lang.equals("German")){
+                greeting = "Hallo ";
+            } else if (lang.equals("Klingon")) {
+                greeting = "There is no Klingon word for 'Hello', ";
+            }
+            return greeting;
+        }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String greeting = request.getParameter("greeting");
+        String message = createMessage(greeting);
+        return name + message;
+
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name){
+        return "Hello " + name;
+    }
 
     @RequestMapping(value = "")
     @ResponseBody
@@ -20,29 +73,6 @@ public class HelloController {
         if (name == null) {
             name = "World";
         }
-        return "Hello " + name;
-    }
-
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloForm(){
-        String html = "<form method='post'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet Me!'/>" +
-                "</form>";
-        return html;
-    }
-
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(HttpServletRequest request){
-        String name = request.getParameter("name");
-        return "Hello " + name;
-    }
-
-    @RequestMapping(value = "hello/{name}")
-    @ResponseBody
-    public String helloUrlSegment(@PathVariable String name){
         return "Hello " + name;
     }
 
